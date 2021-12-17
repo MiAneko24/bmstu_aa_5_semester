2e5bafc88861b1933eec271e12b2499780c3af7b
MiAneko <mariya.serova2010@yandex.ru>
2021-12-17 14:20:33 +0300

    Report 06

--- lab_06/code/main.cpp
+++ lab_06/code/main.cpp
@@ -98,6 +98,7 @@ vector<double> get_probability(size_t from, vector<size_t> to, vector<vector<dou
 		chisl = pow(tao[from][to[j]], alpha) * pow(attraction[from][to[j]], beta);
 		result[j] = chisl / znam;
 	}
+
 	return result;
 }
 
@@ -217,38 +218,38 @@ int main()
 	}
 	fclose(f);
 	
-	size_t n2;
-	f = fopen("test2.txt", "r");
-	if (f == NULL) {
-		cout << "ERROR_READ_FILE" << endl;
-		return 0;
-	}
-	fscanf(f, "%ld", &n2);
-	cout << n << endl;
-	vector<vector<size_t>> d2(n2);
-	for (size_t i = 0; i < n2; i++) {
-		d2[i].resize(n2);
-		for (size_t j = 0; j < n2; j++)
-			fscanf(f, "%ld", &d2[i][j]);
-	}
-	fclose(f);
+	// size_t n2;
+	// f = fopen("test2.txt", "r");
+	// if (f == NULL) {
+	// 	cout << "ERROR_READ_FILE" << endl;
+	// 	return 0;
+	// }
+	// fscanf(f, "%ld", &n2);
+	// cout << n << endl;
+	// vector<vector<size_t>> d2(n2);
+	// for (size_t i = 0; i < n2; i++) {
+	// 	d2[i].resize(n2);
+	// 	for (size_t j = 0; j < n2; j++)
+	// 		fscanf(f, "%ld", &d2[i][j]);
+	// }
+	// fclose(f);
 	
 
-	size_t n3;
-	f = fopen("test4.txt", "r");
-	if (f == NULL) {
-		cout << "ERROR_READ_FILE" << endl;
-		return 0;
-	}
-	fscanf(f, "%ld", &n3);
-	cout << n3 << endl;
-	vector<vector<size_t>> d3(n3);
-	for (size_t i = 0; i < n3; i++) {
-		d3[i].resize(n3);
-		for (size_t j = 0; j < n3; j++)
-			fscanf(f, "%ld", &d3[i][j]);
-	}
-	fclose(f);
+	// size_t n3;
+	// f = fopen("test4.txt", "r");
+	// if (f == NULL) {
+	// 	cout << "ERROR_READ_FILE" << endl;
+	// 	return 0;
+	// }
+	// fscanf(f, "%ld", &n3);
+	// cout << n3 << endl;
+	// vector<vector<size_t>> d3(n3);
+	// for (size_t i = 0; i < n3; i++) {
+	// 	d3[i].resize(n3);
+	// 	for (size_t j = 0; j < n3; j++)
+	// 		fscanf(f, "%ld", &d3[i][j]);
+	// }
+	// fclose(f);
 
 
 	// for (size_t i = 0; i < n; i++) {
@@ -268,65 +269,65 @@ int main()
 	cout << endl << "Путь: ";
 	print_arr(route_min);
 	cout << "Длина: " << l_min << endl << endl;
-	size_t l_force1 = l_min;
-	end = 0;
-	for (int i = 0; i < cnt; i++){
-		beg = clock();
-		brutforce(n2, d2);
-		end += (clock()-beg)/ CLOCKS_PER_SEC;
-	}
-	cout << "Время работы полного перебора: " << end/cnt  << endl;
+	// size_t l_force1 = l_min;
+	// end = 0;
+	// for (int i = 0; i < cnt; i++){
+	// 	beg = clock();
+	// 	brutforce(n2, d2);
+	// 	end += (clock()-beg)/ CLOCKS_PER_SEC;
+	// }
+	// cout << "Время работы полного перебора: " << end/cnt  << endl;
 
-	cout << endl << "Путь: ";
-	print_arr(route_min);
-	cout << "Длина: " << l_min << endl << endl;
-	size_t l_force2 = l_min;
-	end = 0;
-	for (int i = 0; i < cnt; i++){
-		beg = clock();
-		brutforce(n3, d3);
-		end += (clock()-beg)/ CLOCKS_PER_SEC;
-	}
-	cout << "Время работы полного перебора: " << end/cnt  << endl;
+	// cout << endl << "Путь: ";
+	// print_arr(route_min);
+	// cout << "Длина: " << l_min << endl << endl;
+	// size_t l_force2 = l_min;
+	// end = 0;
+	// for (int i = 0; i < cnt; i++){
+	// 	beg = clock();
+	// 	brutforce(n3, d3);
+	// 	end += (clock()-beg)/ CLOCKS_PER_SEC;
+	// }
+	// cout << "Время работы полного перебора: " << end/cnt  << endl;
 
-	cout << endl << "Путь: ";
-	print_arr(route_min);
-	cout << "Длина: " << l_min << endl << endl;
-	size_t l_force3 = l_min;
+	// cout << endl << "Путь: ";
+	// print_arr(route_min);
+	// cout << "Длина: " << l_min << endl << endl;
+	// size_t l_force3 = l_min;
 
 	vector<double> alphas = { 0.0, 0.25, 0.5, 0.75, 1};
 	vector<double> qs = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 };
 	vector<size_t> tmax = { 50, 100, 150, 200, 250, 300, 350, 400};
 
-	ofstream result("result.txt");
+	// ofstream result("result.txt");
 
-	for (size_t time = 0; time < tmax.size(); time++)
-		for (size_t koef = 0; koef < alphas.size(); koef++)
-			for (size_t isp = 0; isp < qs.size(); isp++) {
-				ant(n, d, alphas[koef], 1 - alphas[koef], qs[isp], tmax[time]);
+	// for (size_t time = 0; time < tmax.size(); time++)
+	// 	for (size_t koef = 0; koef < alphas.size(); koef++)
+	// 		for (size_t isp = 0; isp < qs.size(); isp++) {
+	// 			ant(n, d, alphas[koef], 1 - alphas[koef], qs[isp], tmax[time]);
 
-				result << " & " << tmax[time] << " & " << alphas[koef] << " & " << 1-alphas[koef] << " & " << qs[isp] << " & " << abs(long(l_min-l_force1));
-				ant(n2, d2, alphas[koef], 1 - alphas[koef], qs[isp], tmax[time]);
-				result << " & " << abs(long(l_min-l_force2));
-				ant(n3, d3, alphas[koef], 1 - alphas[koef], qs[isp], tmax[time]);
-				result << " & " << abs(long(l_min-l_force3));
-				result << " \\\\ \n" << endl;
-			}
+	// 			result << " & " << tmax[time] << " & " << alphas[koef] << " & " << 1-alphas[koef] << " & " << qs[isp] << " & " << abs(long(l_min-l_force1));
+	// 			ant(n2, d2, alphas[koef], 1 - alphas[koef], qs[isp], tmax[time]);
+	// 			result << " & " << abs(long(l_min-l_force2));
+	// 			ant(n3, d3, alphas[koef], 1 - alphas[koef], qs[isp], tmax[time]);
+	// 			result << " & " << abs(long(l_min-l_force3));
+	// 			result << " \\\\ \n" << endl;
+	// 		}
 
-	result.close();
+	// result.close();
 
 	//0, 1, 0.9, 150
-	// end = 0;
-	// for (int i = 0; i < cnt; i++){
-	// 	beg = clock();
-	// 	ant(n, d, 0, 1, 0.9, 150);
-	// 	end += (clock()-beg)/ CLOCKS_PER_SEC;
-	// }
-	// cout << "Время работы муравьиного алгоритма: " << end / cnt << endl;
+	end = 0;
+	for (int i = 0; i < cnt; i++){
+		beg = clock();
+		ant(n, d, 0, 1, 0.9, 150);
+		end += (clock()-beg)/ CLOCKS_PER_SEC;
+	}
+	cout << "Время работы муравьиного алгоритма: " << end / cnt << endl;
 
-	// cout << endl << "Путь: ";
-	// print_arr(route_min);
-	// cout << "Длина: " << l_min << endl << endl;
+	cout << endl << "Путь: ";
+	print_arr(route_min);
+	cout << "Длина: " << l_min << endl << endl;
 
 	return 0;
 }
